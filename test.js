389eaff15a131d8a94064834e3cd42492fa9ab8b
Eliott <eliott.benoit4@gmail.com>
2016-02-22 13:58:31 +0000

    cleaning

--- test.js
+++
@@ -1,7 +0,0 @@
-var fs = require("fs");
-var users = require("./users.json");
-var tweets = require("./tweets.json");
-
-for(var i = 0; i < users.length ; i ++) {
-	console.log("User : " + users[i]);
-}
